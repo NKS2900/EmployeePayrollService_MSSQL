40c704a7e94ca30cd1533cff3579f64776768c90
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-26 17:47:32 +0530

    [NIJAM] Add:Alter and Update Table Data.

---
+++ AlterTable.sql
@@ -0,0 +1,4 @@
+alter table [dbo].[employee_payroll] add gender char(1)
+
+update [dbo].[employee_payroll] set gender = 'M' where name = 'john' or name = 'bob'
+update [dbo].[employee_payroll] set gender = 'F' where name = 'michel' or name= 'smith'
