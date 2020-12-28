bbdec73eadf51c2e9f553ff93c639a67964301a3
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-28 17:21:07 +0530

    [NIJAM] Add:Terissa is part of Sales and Marketing department with diff. Id.

---
+++ TerissaInserted.sql
@@ -0,0 +1,5 @@
+insert into employee_payroll 
+values(104,'Terissa',400000.00,'2020-06-1','F',7418529630,'Sales','Mumbai',2000,1000,200,18000),
+(105,'Terissa',400000.00,'2020-06-1','F',7418529630,'Marketing','Mumbai',2000,1000,200,18000);
+
+select * from employee_payroll;
