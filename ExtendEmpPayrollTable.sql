d0fbdac1e6935891d0a22e972302250446631bf6
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-28 17:08:58 +0530

    [NIJAM] Add:Extend Employee Payroll Table To have Basic pay,Deductions,Taxable pay ,Net pay.

---
+++ ExtendEmpPayrollTable.sql
@@ -0,0 +1,8 @@
+sp_rename 'employee_payroll.salary','basic_pay'
+alter table employee_payroll add
+Deduction float,
+Taxable_Pay real,
+Income_Tax float,
+Net_Pay real;
+
+select * from employee_payroll;
